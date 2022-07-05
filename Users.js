e4dfe9aee27ee3685758b4a3e4f0a6e4b1badfe8
Hassan Lamidi <hassanadefarati@gmail.com>
2022-07-05 01:06:52 +0100

    first commit

---
+++ Users.js
@@ -0,0 +1,24 @@
+const users = [
+  {
+    id: 1,
+    name: "Hassan",
+    email: "hassanadefarati@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Hussein",
+    email: "husseinademiluyi1@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Taiwo",
+    email: "taiwo@gmail.com",
+  },
+  {
+    id: 4 ,
+    name: "Kehinde",
+    email: "kehinde@gmail.com",
+  },
+];
+
+module.exports = users;
